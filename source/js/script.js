76c0974eddec4c79bbb7ac0d04c27bc428906760
Bodal Andrey <a-v-bodal@yandex.ru>
2019-11-29 16:26:54 +0500

    Адаптивные декоративные элементы

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,11 @@ var navButton = document.querySelector(".main-nav__button");
 navMain.classList.remove(main-nav--nojs);
 
 navButton.addEventListener("click", function() {
-   if (navMain.classList.contains("main-nav--closed")) {
-       navMain.classList.remove("main-nav--closed");
-       navMain.classList.add("main-nav--opened");
-   } else {
-       navMain.classList.add("main-nav--closed");
-       navMain.classList.remove("mai-nav--opened");
+    if (navMain.classList.contains("main-nav--closed")) {
+        navMain.classList.remove("main-nav--closed");
+        navMain.classList.add("main-nav--opened");
+    } else {
+        navMain.classList.add("main-nav--closed");
+        navMain.classList.remove("mai-nav--opened");
    }
 });
